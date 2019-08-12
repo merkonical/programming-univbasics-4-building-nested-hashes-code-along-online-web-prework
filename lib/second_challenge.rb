3ffe51f565553f1c54351aaacadd6381617db2ca
Maverick Adolfo <merkonical@gmail.com>
2019-08-12 07:43:22 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,10 +5,12 @@ def second_challenge
       :matriarch => {},
       :hero => {},
       :hero_friends => []
-      }, 
+    }, 
     :capulet => {
-      #your key/value pairs here!
-      }}
-
-  
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+    }
+  }
 end
